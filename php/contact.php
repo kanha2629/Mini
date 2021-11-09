7db7382aa2399001cade225425c2f81a625fec7f
Jisha Anna Philip <jishaannphilip@gmail.com>
2021-11-09 23:36:19 +0530

    Contact form

--- php/contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("DB_HOST","localhost");
-define("DB_USER","root");
-define("DB_PASSWORD","");
-define("DB_NAME","sahay");
-
-$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-?>
--- php/process.php
+++ php/process.php
@@ -1,17 +1,32 @@
 <?php
+$server = 'localhost'; 
+$username = 'root'; 
+$password=''; 
+$dbname = "sahay";
+$conn = mysqli_connect($server, $username, $password, $dbname);
 
-include("contact.php");
-extract($_POST);
-$sql = "INSERT INTO `contact-data` (`name`,`email`,`message`) VALUES ('".$name."','".$email."','".$message."')";
-$result = $myqli->query($sql);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  
+    // collect value of input field
+    $name = $_REQUEST['name'];
+    $email = $_REQUEST['email'];
+    $message = $_REQUEST['message'];
+  
+    if (empty($name)||(empty($email))||(empty($message))) {
+        echo "Please fill all details";
+    } else {
+        $sql = "INSERT INTO `contact-data` (`name`,`email`,`message`) VALUES ('".$name."','".$email."','".$message."')";
+        $run = mysqli_query($conn,$sql);
+    
 
+    echo "Thank You For Contacting Us ";
+    }}
+    $conn->close();
 
-if(!$result){
-    die("Couldn't enter data: ".$mysqli->error);
-}
+    
+        
+    
 
-echo "Thank You For Contacting Us ";
-$mysqli->close();
 
 
 ?>
