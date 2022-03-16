3ff3196c2a685b992f5453895ceda7498e09373a
kanha2629 <2019kanaiya.kanabar@ves.ac.in>
2022-03-16 13:58:37 +0530

    s

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', async event => {
+    console.log('install event')
+  });
+  
+  self.addEventListener('fetch', async event => {
+    console.log('fetch event')
+  });
+
+  
+const cacheName = 'pwa-conf-v1';
+const staticAssets = [
+'./',
+'./index.html',
+'./app.js',
+'./styles.css'
+];
+
+
+self.addEventListener('install', async event => {
+    const cache = await caches.open(cacheName); (1)
+    await cache.addAll(staticAssets); (2)
+  });
