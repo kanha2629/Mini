9eee98ca93738b56f2e95cde4d567707673138cb
kanha2629 <2019kanaiya.kanabar@ves.ac.in>
2022-03-16 14:04:04 +0530

    K

--- sw.js
+++ sw.js
@@ -17,6 +17,45 @@ const staticAssets = [
 
 
 self.addEventListener('install', async event => {
+const cache = await caches.open(cacheName); (1)
+await cache.addAll(staticAssets); (2)
+});
+
+self.addEventListener('fetch', event => {
+const req = event.request;
+event.respondWith(cacheFirst(req));
+});
+
+    async function cacheFirst(req) {
     const cache = await caches.open(cacheName); (1)
-    await cache.addAll(staticAssets); (2)
-  });
+    const cachedResponse = await cache.match(req); (2)
+    return cachedResponse || fetch(req); (3)
+    }
+
+    self.addEventListener('fetch', event => {
+    const req = event.request;
+
+    if (/.*(json)$/.test(req.url)) {
+        event.respondWith(networkFirst(req));
+    } else {
+        event.respondWith(cacheFirst(req));
+    }
+    });
+
+    async function networkFirst(req) {
+        const cache = await caches.open(cacheName);
+        try { (1)
+          const fresh = await fetch(req);
+          cache.put(req, fresh.clone());
+          return fresh;
+        } catch (e) { (2)
+          const cachedResponse = await cache.match(req);
+          return cachedResponse;
+        }
+      }
+
+async function cacheFirst(req) {
+const cache = await caches.open(cacheName);
+const cachedResponse = await cache.match(req);
+return cachedResponse || networkFirst(req);
+}
